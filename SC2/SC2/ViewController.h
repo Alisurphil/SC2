e1efa9ae9c0161a6f627fe4c7c38f915a528d737
wx <770521935@qq.com>
2015-11-24 11:30:52 +0800

    创建项目

---
+++ SC2/SC2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SC2
+//
+//  Created by wx on 15/11/24.
+//  Copyright © 2015年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
