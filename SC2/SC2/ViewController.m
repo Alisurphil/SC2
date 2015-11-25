ae584a4d7be92080c55436a8ea92fb8314086716
wx <770521935@qq.com>
2015-11-25 14:24:27 +0800

    no message

--- SC2/SC2/ViewController.m
+++ SC2/SC2/ViewController.m
@@ -25,19 +25,35 @@
 //            NSLog(@"成功");
 //        }
 //    }];
-    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name == '测试'"];
-    PFQuery *query = [PFQuery queryWithClassName:@"announcement" predicate:predicate];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        PFObject *annou = objects.firstObject;
-//        NSLog(@"%@", annou[@"content"]);
-//        annou[@"content"] = @"不测试不测试";
-//        [annou saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+    
+//    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"content == '不测试不测试'"];
+//    PFQuery *query = [PFQuery queryWithClassName:@"announcement" predicate:predicate];
+//    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+//        PFObject *annou = objects[0];
+//    
+//       NSLog(@"%@", annou[@"name"]);
+//        annou[@"name"] = @"不测试";
+//       [annou saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
 //            NSLog(@"成功");
 //        }];
-        [annou deleteInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
-            NSLog(@"成功");
-        }];
-    }];
+//
+//        [annou deleteInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+//            NSLog(@"成功");
+//        }];
+//    }];
+    PFObject *myPost = [PFObject objectWithClassName:@"announcement"];
+    myPost[@"name"] = @"I'm Hungry";
+    myPost[@"content"] = @"Where should we go for lunch?";
+    
+    // Create the comment
+    PFObject *myComment = [PFObject objectWithClassName:@"Comment"];
+    myComment[@"content"] = @"Let's do Sushirrito.";
+    
+    // Add a relation between the Post and Comment
+    myComment[@"parent"] = myPost;
+    
+    // This will save both myPost and myComment
+    [myComment saveInBackground];
 }
 
 - (void)didReceiveMemoryWarning {
