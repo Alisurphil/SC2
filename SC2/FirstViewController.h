983c3d9534777ba43a2cb3911eec7a81c09f877f
袁文轶 <ywy4869@live.cn>
2015-11-26 08:44:59 +0800

    三个大页面，以及登陆界面和首页详情

---
+++ SC2/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  SC2
+//
+//  Created by 袁文轶 on 15/11/25.
+//  Copyright © 2015年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ SC2/SCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCViewController.h
+//  SC2
+//
+//  Created by 袁文轶 on 15/11/25.
+//  Copyright © 2015年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCViewController : UITabBarController
+
+@end
