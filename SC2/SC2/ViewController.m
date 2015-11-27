d3f67333968cc77475fa757012ef2a9714e57f5f
谢雪彬 <q131968850@qq.com>
2015-11-27 15:34:55 +0800

    注册

--- SC2/SC2/ViewController.m
+++ SC2/SC2/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <EaseMobSDKFull/EaseMob.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do any additional setup after loading the view, typically from a nib.
 //    PFObject *annou = [PFObject objectWithClassName:@"announcement"];
 //    annou[@"name"] = @"测试";
