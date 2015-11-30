e7cbcc227a5e5b88206487b224311c1c3b24e562
wx <770521935@qq.com>
2015-11-30 09:07:17 +0800

    no message

--- SC2/LeftViewController.m
+++ SC2/LeftViewController.m
@@ -50,4 +50,53 @@
     //在视图上展示
     [actionSheet showInView:self.view];
 }
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:                                                       (NSInteger)buttonIndex {
+    // 相册 0 拍照 1
+    switch (buttonIndex) {         case 0:
+            //从相册中读取
+            [self readImageFromAlbum];
+            break;
+        case 1:
+            //拍照
+        [self readImageFromCamera];
+        break;
+        default:
+        break;
+    } }
+//从相册中读取
+- (void)readImageFromAlbum {
+    //创建对象
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    //（选择类型）表示仅仅从相册中选取照片
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    //指定代理，因此我们要实现UIImagePickerControllerDelegate,UINavigationControllerDelegate协议
+    imagePicker.delegate = self;
+    //设置在相册选完照片后，是否跳到编辑模式进行图片剪裁。(允许用户编辑)
+    imagePicker.allowsEditing = YES;
+    //显示相册
+    [self presentViewController:imagePicker animated:YES completion:nil];
+}
+- (void)readImageFromCamera {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];         imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        imagePicker.delegate = self;
+        imagePicker.allowsEditing = YES;
+        //允许用户编辑
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    } else {
+            //弹出窗口响应点击事件
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"警告"message:@"未检测到摄像头" delegate:         nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil];
+        [alert show];
+    }
+}
+//图片完成之后处理
+- (void)imagePickerController:(UIImagePickerController *)picker
+        didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo {
+    //image 就是修改后的照片
+    //将图片添加到对应的视图上
+    [:image forState:UIControlStateNormal];
+    //结束操作
+    [self dismissViewControllerAnimated:YES completion:nil]; }
 @end
