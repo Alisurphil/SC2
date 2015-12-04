25cae3fd81c8f82a08847f1be1b044519c3a0b85
谢雪彬 <q131968850@qq.com>
2015-12-04 16:44:03 +0800

    环信集成

--- SC2/SCViewController.h
+++ SC2/SCViewController.h
@@ -11,17 +11,56 @@
 #import "SecondCollectionViewController.h"
 #import "ThirdTableViewController.h"
 #import "LoginViewController.h"
-@interface SCViewController : UITabBarController<UITabBarControllerDelegate>
+#import "SecondViewController.h"
+#import "ThirdViewController.h"
+#import "ChatListViewController.h"
+#import "ContactsViewController.h"
+#import "ChatViewController.h"
+#import "EMCDDeviceManager.h"
+#import "ApplyViewController.h"
+#import "UserProfileManager.h"
+#import "CallViewController.h"
+#import "TTGlobalUICommon.h"
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+static const CGFloat kDefaultPlaySoundInterval = 3.0;
+static NSString *kMessageType = @"MessageType";
+static NSString *kConversationChatter = @"ConversationChatter";
+static NSString *kGroupName = @"GroupName";
+
+@interface SCViewController : UITabBarController<UITabBarControllerDelegate> {
+    EMConnectionState _connectionState;
+    ChatListViewController *_chatListVC;
+    ContactsViewController *_contactsVC;
+    UIBarButtonItem *_addFriendItem;
+
+}
 
 @property (strong, nonatomic) LoginViewController *loginVC;
 
 @property (strong,nonatomic) UINavigationController *FirstNC;
 @property (strong,nonatomic) FirstViewController *FirstVC;
 
-@property (strong,nonatomic) UINavigationController *SecondNC;
+
 @property (strong,nonatomic) SecondCollectionViewController *SecondVC;
 
 @property (strong,nonatomic) UINavigationController *ThirdNC;
 @property (strong,nonatomic) ThirdTableViewController *ThirdVC;
+
+@property (strong,nonatomic) UINavigationController *SecondNCA;
+@property (strong,nonatomic) UINavigationController *SecondNCB;
+@property (strong,nonatomic) UINavigationController *SecondNCC;
+
+@property (strong, nonatomic) NSDate *lastPlaySoundDate;
+
+- (void)jumpToChatList;
+
+- (void)setupUntreatedApplyCount;
+
+- (void)networkChanged:(EMConnectionState)connectionState;
+
+- (void)didReceiveLocalNotification:(UILocalNotification *)notification;
+
 
 @end
