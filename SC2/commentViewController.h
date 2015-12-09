27c0894960ebef07e97951b5cba68fda66e9123b
姚国俊 <467458706@qq.com>
2015-12-09 14:34:03 +0800

    no message

--- SC2/commentViewController.h
+++ SC2/commentViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface commentViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong,nonatomic) PFObject *nextName;
+@property (strong,nonatomic) NSArray *allComment;
 @end
